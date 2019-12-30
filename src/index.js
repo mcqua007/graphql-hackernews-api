0bde073d481a09001178e061cb7505adfd04824f
Sean McQuaid <sean@Seans-MacBook-Pro.local>
2019-12-29 19:06:01 -0800

    type: feat | added subscription resolver

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
 
 const resolvers = {
   Query,
   Mutation,
   User,
-  Link
+  Link,
+  Subscription
 }
 
 
